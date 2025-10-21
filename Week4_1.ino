483bbc57faf680367fe322fcabda3d34a283ee14
veevekmgr9 <80833475+veevekmgr9@users.noreply.github.com>
2025-10-21 13:38:36 +0100

    Add comments for clarity in Week4_1.ino
    Added comments to clarify pin initialization, serial communication, ADC reading, and voltage conversion.

--- Week4_1.ino
+++ Week4_1.ino
@@ -1,21 +1,24 @@
-int pin = A0;
+int pin = A0; //initialize of the pin A0
 
 void setup() {
-  Serial.begin(9600);
-  while(!Serial);
+  Serial.begin(9600);//start serial communication
+  while(!Serial);//wait until serial monitor gets ready
 }
 
 void loop() {
-  uint16_t rawADC = analogRead(pin);
-  float voltage = 3.3*rawADC/1023.0;
+  uint16_t rawADC = analogRead(pin);  // Read the raw analog value (0–1023) from the specified analog input pin
+  float voltage = 3.3*rawADC/1023.0;  // Convert the raw ADC value to voltage (assuming a 3.3V reference)
 
-  Serial.print("ADC Value = ");
+   // Print the raw ADC value to the Serial Monitor
+  Serial.print("ADC Value = "); 
   Serial.print(rawADC);
   Serial.println(".");
 
+   // Print the voltage value to the Serial Monitor
   Serial.print("Voltage = ");
   Serial.println(voltage);
   Serial.println(" V.");
 
+  //Wait 1 sec before repeating the loop
   delay(1000);
 }
